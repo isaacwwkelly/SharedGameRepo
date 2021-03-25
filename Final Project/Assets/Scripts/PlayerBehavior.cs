927b87271391053fa7e7b1d310d6019ca9416ee3
isaacwwkelly <49201222+isaacwwkelly@users.noreply.github.com>
2021-03-24 22:19:30 -0500

    Added platform tag and edited playerbehavior.cs

--- Final Project/Assets/Scripts/PlayerBehavior.cs
+++ Final Project/Assets/Scripts/PlayerBehavior.cs
@@ -6,7 +6,7 @@ public class PlayerBehavior : MonoBehaviour
 {
     [SerializeField] private int speed;
     [SerializeField] private float jumpIntensity;
-    [SerializeField] private bool isGrounded = true;
+    [SerializeField] private bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +21,23 @@ public class PlayerBehavior : MonoBehaviour
         transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, 1) * speed * Time.deltaTime;
 
         // Allow player to jump, only if the player is grounded on a platform
-        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)))
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && isGrounded)
             gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpIntensity, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "platform")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "platform")
+        {
+            isGrounded = false;
+        }
     }
 }
