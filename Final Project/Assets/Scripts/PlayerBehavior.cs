72d12584e329b95b3c67ca2e0a9e7010ab2f6297
isaacwwkelly <49201222+isaacwwkelly@users.noreply.github.com>
2021-03-24 20:54:29 -0500

    Added Materials Folder
    And a playerbehavior.cs script

--- Final Project/Assets/Scripts/PlayerBehavior.cs
+++ Final Project/Assets/Scripts/PlayerBehavior.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
+    [SerializeField] private int speed;
+    [SerializeField] private int jumpIntensity;
+    [SerializeField] private bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,11 @@ public class PlayerBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Left and right control on player
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, 1) * speed * Time.deltaTime;
+
+        // Allow player to jump, only if the player is grounded on a platform
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && isGrounded)
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpIntensity, ForceMode.Impulse);
     }
 }
